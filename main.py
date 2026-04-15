1cccae3ad41e9442e49fc69e8b6eb0bc7c713d52
Your Name <bentleyc0424@gmail.com>
2026-04-15 16:25:10 -0500

    code thingy is closer to finishing :)

--- main.py
+++ main.py
@@ -1,58 +1,64 @@
 from turtle import *
 import random
-screen = Screen()
-screen.bgcolor("black")
-screen.setup(520,520)
-
 def generate_color():
     return f"#{random.randint(0, 0xFFFFFF):06x}"
 
-
-
-
-turtle1 = Turtle()
-turtle1.shape("triangle")
-turtle1.color("green")
-
-
-
+def move_heading(t):
+    t.forward(5)
+    if t.xcor() > 240:
+        t.setheading(180)
+    if t.xcor() < -240:
+        t.setheading(0)
+   
 
 def playing_area():
-    Turtle.begin_fill()
-    for i in range(sides):
-        turtle.forward(50)
-        turtle.left(360/ sides)
-    turtle.end_fill()
+    pen = Turtle()
+    pen.color("yellow")
+    pen.begin_fill()
+    pen.goto(-240,240)
+    pen.goto(240,240)
+    pen.goto(240,-240)
+    pen.goto(-240,-240)
+    pen.goto(240,240)
+    pen.end_fill()
+
+
+screen = Screen()
+screen.bgcolor("purple")
+screen.setup(520,520)
+
+playing_area()
 
 
 
 
-def move_with_heading(t, turtles):
-    direction = input("enter up,left,down,or right")
-    if input == "up":
-        turtle1.up(50)
-    elif input == "left":
-        turtle1.left(50)
-    elif input == "down":
-        turtle1.down(50)
-    elif input == "right":
-        turtle1.right(50)
+yertle = Turtle() 
+yertle.color("red")
+yertle.speed(0)
+yertle.shape("turtle")
+deltax = random.randint(-2,2)
+deltay = random.randint(-2,2)
+
+def move_xy(turtle, deltax, deltay):
+    newy = turtle.ycor()+ deltay
+    if newy > 240 or newy < -240:
+        newy = turtle.ycor()
+        deltay *= -1
 
 
+    newx = turtle.xcor()+ deltax
+    if newx > 240 or newx < -240:
+        newx = turtle.xcor()
+        deltax *= -1
+    turtle.goto(newx, newy)
 
+    return deltax, deltay
 
+alive = True
+while alive:
+    deltax, delay = move_xy(yertle,deltax,deltay)
 
-def move_with_deltas(t, dx, dy):
-    pass
-
-
-
-
-
-
-
+    
 
 
 screen.exitonclick()
-playing_area()
-move_with_heading(t, turtles)
